068a398a5105fc8dfc24c2170c3b42e8e2ca8793
DavidDsz <daviddaszynski@hotmail.de>
2022-12-19 17:02:14 +0100

    Changes

--- src/views/Items.vue
+++ src/views/Items.vue
@@ -2,9 +2,7 @@
   <h1>Welcome to your Shopping List</h1>
   <div class="container-fluid">
     <div class="row row-cols-1 row-cols-md-4 g-4">
-      <div class="col" v-for="item in item" :key="item.id">
-        <div class="card h-100">
-          <img :src="getAvatar(item)" class="card-img-top" :alt="item.produkt + ' ' + item.menge">
+      <div class="col" v-for="item in items" :key="item.id">
           <div class="card-body">
             <h5 class="card-title">{{ item.produkt }} {{ item.menge }}</h5>
             <p class="card-text">
@@ -14,7 +12,6 @@
         </div>
       </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -33,7 +30,9 @@ export default {
 
     fetch('http://localhost:8080/api/v1/item', requestOptions)
       .then(response => response.json())
-      .then(result => console.log(result))
+      .then(result => result.forEach(item => {
+        this.items.push(item)
+      }))
       .catch(error => console.log('error', error))
   }
 }
