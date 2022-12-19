6fd18712937e3684c23c9c799f95cd334ef8331a
DavidDsz <daviddaszynski@hotmail.de>
2022-12-19 17:39:06 +0100

    Changes

--- src/views/Items.vue
+++ src/views/Items.vue
@@ -4,9 +4,9 @@
     <div class="row row-cols-1 row-cols-md-4 g-4">
       <div class="col" v-for="item in items" :key="item.id">
           <div class="card-body">
-            <h5 class="card-title">{{ item.produkt }} {{ item.menge }}</h5>
+            <h5 class="card-title">{{ item.produkt }}</h5>
             <p class="card-text">
-              Produkt: {{ item.produkt }} Menge: {{ item.menge }}
+              Menge: {{ item.menge }}
             </p>
           </div>
         </div>
