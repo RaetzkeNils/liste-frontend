b2ef830bb836084a228515d4f1a350dba777e697
RaetzkeNils <nils0910@hotmail.de>
2023-01-08 22:53:09 +0100

    neue about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Das ist unsere About Page</h1>
+    <p>Dies ist das Projekt im Modul Webtechnologien im WiSe 22/23 bei Prof. Dr. Arif Wider von David Daszynski und Nils R&auml;tzke.</p>
+
+    <p>Mit unserer Web-Anwendung stellen wir eine <strong>Einkaufsliste </strong>dar.<br />
+      Der Nutzer kann diese unter dem Reiter Items erstellen. Mit einem Klick auf &quot;Create&quot; &ouml;ffnen sich die zwei Zeilen Product und Quantity.<br />
+      Hier kann der Nutzer dann seine entsprechend noch einzukaufenden Produkte eintragen. Er kann sie im Nachhinein ebenso noch bearbeiten oder l&ouml;schen.</p>
+
+    <p>&nbsp;</p>
   </div>
 </template>
